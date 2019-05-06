b65c11963e23ee40f56f77b97a93cd9469a16d03
tuayang <tuayang@gmail.com>
2019-05-05 22:28:55 -0700

    Updates

---
+++ precache-manifest.089404c40f82ad72d13e55dcceea85a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e929cc14b6b10cb0fd57e17c0bf6dda4",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "617d7480a1bac814d442",
+    "url": "/clickygame/static/css/main.185c31f1.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "617d7480a1bac814d442",
+    "url": "/clickygame/static/js/main.1a0d581a.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
